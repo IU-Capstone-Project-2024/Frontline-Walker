ff7a49effd2ed1960e1191ada3b715addbe481a5
NikitaRUNI <cherkashin-nikita22@mail.ru>
2024-06-22 13:12:15 +0300

    Binoculars mechanic and base camera implementation
    All variables are stored in camera and binoculars objects.

--- FrontlineWalker/Assets/Scripts/TestScripts/TestPlayer (Nikita).cs
+++ FrontlineWalker/Assets/Scripts/TestScripts/TestPlayer (Nikita).cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
+    public Camera cameraObject;
+    private cameraInit cameraInit;
+
+    void Start()
+    {
+        cameraInit = cameraObject.GetComponent<cameraInit>();
+    }
 
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if (!cameraInit.binocularsActive)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            transform.Translate(movement * moveSpeed * Time.deltaTime);
+        }
     }
 }
