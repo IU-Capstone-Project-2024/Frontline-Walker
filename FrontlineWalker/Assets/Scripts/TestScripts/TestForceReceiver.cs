c31946c087a49d801cdcf1489ecb8b781fe42ebf
Andrei Sobolev <108589486+Barabacha474@users.noreply.github.com>
2024-07-07 13:43:46 +0300

    small code refactoring

---
+++ FrontlineWalker/Assets/Scripts/TestScripts/TestForceReceiver.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestForceReceiver : MonoBehaviour
+{
+
+    public Rigidbody2D forceReceiver;
+
+    public void ReceiveForce(float force, Vector2 direction)
+    {
+        forceReceiver.AddForce(direction * force);
+    }
+}
--- FrontlineWalker/Assets/Scripts/TestScripts/TestProjectileShooter.cs
+++ FrontlineWalker/Assets/Scripts/TestScripts/TestProjectileShooter.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class TESTInoyBobrProjectileShooter : MonoBehaviour
+public class TestProjectileShooter : MonoBehaviour
 {
-    [SerializeField] private Projectile usedProjectile;
-    [SerializeField] private bool useInitialVelocity;
-    [SerializeField] private Vector2 shift;
+    [SerializeField] private Projectile _usedProjectile;
+    [SerializeField] private bool _useInitialVelocity;
+    [SerializeField] private Vector2 _shift;
     private Rigidbody2D _rigidbody;
     private float _timer = 0;
 
@@ -19,14 +20,14 @@ public class TESTInoyBobrProjectileShooter : MonoBehaviour
 
     private void Shoot()
     {
-        Projectile newProjectile = Instantiate(usedProjectile, transform.position + transform.rotation * shift, transform.rotation);
-        if (newProjectile.GetComponent<Rigidbody2D>() && useInitialVelocity)
+        Projectile newProjectile = Instantiate(_usedProjectile, transform.position + transform.rotation * _shift, transform.rotation);
+        if (newProjectile.GetComponent<Rigidbody2D>() && _useInitialVelocity)
         {
             newProjectile.GetComponent<Rigidbody2D>().velocity = _rigidbody.velocity;
         }
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         _timer += Time.deltaTime;
         if (_timer > 0.3)
