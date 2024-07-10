08493c5efe4894ab392c6387a2c7435a7637151a
Andrei Sobolev <108589486+Barabacha474@users.noreply.github.com>
2024-07-10 18:21:52 +0300

    Added limited fuel

--- FrontlineWalker/Assets/Scripts/TestScripts/Controllers/TestController.cs
+++ FrontlineWalker/Assets/Scripts/TestScripts/Controllers/TestController.cs
@@ -7,22 +7,33 @@ using UnityEngine.Serialization;
 [RequireComponent(typeof(Rigidbody2D))]
 public class TestController : MonoBehaviour
 {
+    [Header("Controllers")]
     public TestProceduralWalkerAnimation proceduralWalkerAnimation;
     public TestTorsoController torsoController;
     public TestCannon mainCannonController;
     
+    [Header("Speed")]
     public float forward_speed = 2f;
     public float backward_speed = 1f;
     public float forward_force = 50f;
     public float backward_force = 50f;
     public float distabilization_speed = 3f;
+    [Header("Fuel")] 
+    public bool fuelIsLimited = true;
+    public float tankCapacity = 100;
+    public float idleFuelLoss = 0.1f;
+    public float fuelLossPerForceUnit = 0.1f;
 
+    private float _currentFuelLevel;
+    
     private float _current_y;
     private Rigidbody2D _rb;
     
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+
+        _currentFuelLevel = tankCapacity;
     }
 
     private void FixedUpdate()
@@ -38,6 +49,8 @@ public class TestController : MonoBehaviour
         {
             proceduralWalkerAnimation.ResumeAnimation();
         }
+        IdleFuelLoss();
+        Debug.Log(_currentFuelLevel);
     }
 
     public void Right()
@@ -50,6 +63,7 @@ public class TestController : MonoBehaviour
                 _rb.AddForce(Vector2.right * forward_force);
                 proceduralWalkerAnimation.shakeHeight = 0.01f * _forward_speed;
             }
+            FuelLossPerForceUnit(forward_force);
         }
     }
 
@@ -63,11 +77,45 @@ public class TestController : MonoBehaviour
                 _rb.AddForce(-Vector2.right * backward_force);
                 proceduralWalkerAnimation.shakeHeight = 0.01f * _backward_speed;
             }
+            FuelLossPerForceUnit(backward_force);
         }
     }
 
+    private void IdleFuelLoss()
+    {
+        _currentFuelLevel -= idleFuelLoss * Time.deltaTime;
+        ClampFuel();
+    }
+
+    private void FuelLossPerForceUnit(float _force)
+    {
+        _currentFuelLevel -= fuelLossPerForceUnit * _force * Time.deltaTime;
+        ClampFuel();
+    }
+
+    private void ClampFuel()
+    {
+        if (_currentFuelLevel < 0f) _currentFuelLevel = 0f;
+        if (_currentFuelLevel > tankCapacity) _currentFuelLevel = tankCapacity;
+    }
+
+    public void Refuel(float _amountOfFuel)
+    {
+        _currentFuelLevel += _amountOfFuel;
+        ClampFuel();
+    }
+
+    public float GetFuelLeft()
+    {
+        return _currentFuelLevel;
+    }
+    
     private bool AbleToMove()
     {
+        if (fuelIsLimited && _currentFuelLevel == 0)
+        {
+            return false;
+        }
         return torsoController.isStabilized();
     }
 }
---
+++ FrontlineWalker/Assets/Scripts/TestScripts/TestCharacterPart.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCharacterPart : MonoBehaviour
+{
+    [SerializeField] private String name;
+    [Range(0, 1000)]
+    [SerializeField] private int maxHealth = 10;
+    [SerializeField] private bool isCritical;
+
+    private int _health;
+    private bool _isWorking;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (name == null)
+        {
+            name = new string("Unnamed part");
+        }
+
+        _health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ClampHealth()
+    {
+        if (_health > maxHealth) _health = maxHealth;
+        if (_health < 0) _health = 0;
+    }
+
+    private void CheckHealth()
+    {
+        _isWorking = _health > 0;
+    }
+     
+    public int GetHealth()
+    {
+        return _health;
+    }
+
+    public void FixHealth(int _fixAmount)
+    {
+        if (_fixAmount < 0)
+        {
+            throw new Exception("Amount of fix can not be negative!");
+        }
+        _health += _fixAmount;
+        ClampHealth();
+        CheckHealth();
+    }
+    
+    public void TakeDamage(int _damageAmount)
+    {
+        if (_damageAmount < 0)
+        {
+            throw new Exception("Amount of damage can not be negative!");
+        }
+        _health -= _damageAmount;
+        ClampHealth();
+        CheckHealth();
+    }
+
+    public bool IsCritical()
+    {
+        return isCritical;
+    }
+
+    public String GetName()
+    {
+        return name;
+    }
+
+    public bool IsWorking()
+    {
+        return _isWorking;
+    }
+}
