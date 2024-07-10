44f0e942e71cdd3b563f47e79e17e5c04105375d
NikitaRUNI <cherkashin-nikita22@mail.ru>
2024-07-11 00:12:26 +0300

    New slider controls (does not work)

--- FrontlineWalker/Assets/Scripts/TestScripts/TestPlayer (Nikita).cs
+++ FrontlineWalker/Assets/Scripts/TestScripts/TestPlayer (Nikita).cs
@@ -5,23 +5,13 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
-    public Camera cameraObject;
-    private cameraInit cameraInit;
-
-    void Start()
-    {
-        cameraInit = cameraObject.GetComponent<cameraInit>();
-    }
 
     void Update()
     {
-        if (!cameraInit.binocularsActive)
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-            transform.Translate(movement * moveSpeed * Time.deltaTime);
-        }
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 }
---
+++ FrontlineWalker/Assets/Scripts/UI/RadialSlider.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class RadialSlider: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+	public int currentValue;
+	public int angleLimit = 30;
+	public GameObject slideSprite;
+	private bool isPointerDown=false;
+
+    public void Start()
+    {
+        GetComponent<Image>().fillAmount = 0.25f;
+
+        slideSprite.transform.localRotation = Quaternion.Euler(0, 0, 90);
+        slideSprite.transform.localPosition = new Vector2(0, -50);
+    }
+
+    public void OnPointerEnter( PointerEventData eventData )
+	{
+		StartCoroutine( "TrackPointer" );            
+	}
+	
+	public void OnPointerExit( PointerEventData eventData )
+	{
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		isPointerDown= true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		isPointerDown= false;
+	}
+
+	IEnumerator TrackPointer()
+	{
+		var ray = GetComponentInParent<GraphicRaycaster>();
+		var input = FindObjectOfType<StandaloneInputModule>();
+		
+		if( ray != null && input != null )
+		{
+			while( Application.isPlaying )
+			{                    
+				if (isPointerDown)
+				{
+
+					Vector2 localPos;
+					RectTransformUtility.ScreenPointToLocalPointInRectangle( transform as RectTransform, Input.mousePosition, ray.eventCamera, out localPos );
+
+					if (localPos.y <= 0f)
+					{
+						float angle = (Mathf.Atan2(localPos.y, localPos.x) * 180f / Mathf.PI + 180f) / 360f;
+
+						GetComponent<Image>().fillAmount = angle;
+
+						currentValue = Mathf.RoundToInt((angle - 0.25f) * angleLimit * 4);
+
+						slideSprite.transform.localRotation = Quaternion.Euler(0, 0, angle * 360);
+
+                        float angleInRadians = (angle * 360 + 180) * Mathf.Deg2Rad;
+
+                        float x = 50 * Mathf.Cos(angleInRadians);
+                        float y = 50 * Mathf.Sin(angleInRadians);
+
+                        slideSprite.transform.localPosition = new Vector2(x, y);
+                    }
+
+                }
+
+				yield return 0;
+			}        
+		}    
+	}
+
+
+
+
+
+}
