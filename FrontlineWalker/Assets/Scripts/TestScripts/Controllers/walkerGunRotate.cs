0073643acea9ed4f599a515b2a0908a04350a61d
NikitaRUNI <cherkashin-nikita22@mail.ru>
2024-07-15 00:30:10 +0300

    Gun walker aim system

--- FrontlineWalker/Assets/Scripts/TestScripts/Controllers/walkerGunRotate.cs
+++ FrontlineWalker/Assets/Scripts/TestScripts/Controllers/walkerGunRotate.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class walkerGunRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float rotationSpeed = 5f;
+    public float minAngle = -45f;
+    public float maxAngle = 45f;
+    public RectTransform uiArea;
+
+    private Coroutine rotateCoroutine;
+
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 tapPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            tapPosition.z = 0;
+
+            if (IsTapWithinUIArea(Input.mousePosition))
+            {
+                Vector3 direction = tapPosition - transform.position;
+                float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                targetAngle = Mathf.Clamp(targetAngle, minAngle, maxAngle);
+
+                if (rotateCoroutine != null)
+                {
+                    StopCoroutine(rotateCoroutine);
+                }
+
+                rotateCoroutine = StartCoroutine(RotateToAngle(targetAngle));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool IsTapWithinUIArea(Vector2 tapPosition)
     {
-        
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(uiArea, tapPosition, null, out Vector2 localPoint);
+        return uiArea.rect.Contains(localPoint);
+    }
+
+    private IEnumerator RotateToAngle(float targetAngle)
+    {
+        while (true)
+        {
+            float currentAngle = transform.eulerAngles.z;
+            if (currentAngle > 180) currentAngle -= 360;
+            float angle = Mathf.MoveTowardsAngle(currentAngle, targetAngle, rotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            if (Mathf.Approximately(angle, targetAngle))
+                break;
+
+            yield return null;
+        }
+
+        rotateCoroutine = null;
     }
 }
