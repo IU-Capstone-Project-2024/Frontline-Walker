40c195821eeb8b54a248ba9ecda0503c3ee935d3
NikitaRUNI <cherkashin-nikita22@mail.ru>
2024-07-17 16:00:37 +0300

    Cannon shells ui sprites implementation

--- FrontlineWalker/Assets/Scripts/UI/shellsManager.cs
+++ FrontlineWalker/Assets/Scripts/UI/shellsManager.cs
@@ -1,22 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class shellsManager : MonoBehaviour
 {
     public GameObject mainCannon;
+    public GameObject shellsTextGameObject;
+    public RectTransform shellsSprites;
     private TestCannon cannonScript;
+    private float xZero;
 
     [SerializeField] TextMeshProUGUI shellsText;
 
     void Start()
     {
         cannonScript = mainCannon.GetComponent<TestCannon>();
+        Vector2 position = shellsSprites.anchoredPosition;
+        xZero = position.x - (11.5f * 8f);
     }
 
     void Update()
     {
-        shellsText.text = "Shells: " + cannonScript.GetNumberOfRemainingShells().ToString();
+        shellsText.text = "+" + (cannonScript.GetNumberOfRemainingShells() - 8).ToString();
+        if (cannonScript.GetNumberOfRemainingShells() < 9)
+        {
+            shellsTextGameObject.SetActive(false);
+            Vector2 position = shellsSprites.anchoredPosition;
+            Vector2 scale = shellsSprites.sizeDelta;
+            shellsSprites.sizeDelta = new Vector2(23 * cannonScript.GetNumberOfRemainingShells(), scale.y);
+            shellsSprites.anchoredPosition = new Vector2(xZero + (11.5f * cannonScript.GetNumberOfRemainingShells()), position.y);
+        }
+        else
+        {
+            shellsTextGameObject.SetActive(true);
+            Vector2 position = shellsSprites.anchoredPosition;
+            Vector2 scale = shellsSprites.sizeDelta;
+            shellsSprites.sizeDelta = new Vector2(23 * 8, scale.y);
+            shellsSprites.anchoredPosition = new Vector2(xZero + (11.5f * 8f), position.y);
+        }
     }
 }
