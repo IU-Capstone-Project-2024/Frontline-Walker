ba809a06cef73b3b95a3ad6824afa51bff9ea1c7
Andrei Sobolev <108589486+Barabacha474@users.noreply.github.com>
2024-07-21 06:07:37 +0300

    First level (first iteration)

---
+++ FrontlineWalker/Assets/Scripts/TestScripts/MoveBackgroundWithPlayer.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackgroundWithPlayer : MonoBehaviour
+{
+
+    public float movementMultiplier;
+    
+    private Transform _player;
+    private float _playerInitialX;
+    private float _initialX;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _playerInitialX = _player.position.x;
+
+        _initialX = transform.position.x;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(_initialX + _playerInitialX - _player.position.x * movementMultiplier, transform.position.y, transform.position.z);
+    }
+}
